d82adebbaaea0d4fed745f46f09ad876be1e4004
yousefmn91 <67608124+yousefmn91@users.noreply.github.com>
2020-07-04 01:38:20 +0430

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,13 @@
+phrase = 'Parcham balast! '
+
+ph_list = list(phrase)
+
+
+new_ph = ''.join(ph_list[8:10])
+new_ph = new_ph + ''.join ([ph_list [4], ph_list [2],
+                            
+                            ph_list [5], ph_list [6]])
+
+
+
+print (new_ph)
