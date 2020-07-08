2d98cbb3f43606b48e7a3c97d4de073e2a3a281b
yousefmn91 <67608124+yousefmn91@users.noreply.github.com>
2020-07-09 01:15:23 +0430

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,20 @@
+w = input ('yek kalame benevis: ')
+
+v= set ('aaeiou')
+
+f = v.intersection(set (w))
+
+for vowel in found:
+    print (vowel)
+    
+    
+    
+    
+
+
+
+
+
+    
+        
+    
