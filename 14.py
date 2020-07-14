bbb920a25c35ae5ff15645f8bf7295f295893c3e
yousefmn91 <67608124+yousefmn91@users.noreply.github.com>
2020-07-14 15:48:03 +0430

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,5 @@
+person = {'name':'yousef',
+          'family':'mobasserian'}
+def showfullname (firstname , lastname):
+     return f'{firstname}   {lastname}'
+print (showfullname (person['name'],person['family']))
