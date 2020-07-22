c88281e671567899518b2a34378186ab0b8d427e
yousefmn91 <67608124+yousefmn91@users.noreply.github.com>
2020-07-22 22:42:41 +0430

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,16 +1,7 @@
-from datetime import datetime
-import time
-import random
-odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25,
-        27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49,
-        51, 53, 55, 57, 59]
-for i in range (4):
-    right_this_minute = datetime.today().minute
-    if right_this_minute in odds:
-        print ("odd minute")
-    else:
-        print ("Not an odd minute")
-    wait_time = random.randit(1,60)
-    time.sleep (wait_time)
+shoa = int(input('Enter a number: '))
+ertefa = int(input('Enter a number: '))
+hajm = shoa*ertefa*2
+masahat = (shoa*2*ertefa)+(shoa*2**2)
+print('hajm: ',hajm)
+print('masahat: ', masahat)
 
-    
