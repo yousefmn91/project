664d25355509af6b7d7c2305c10acfad2b71bf3e
yousefmn91 <67608124+yousefmn91@users.noreply.github.com>
2020-07-22 22:43:51 +0430

    Add files via upload

---
+++ 1,1.py
@@ -0,0 +1,7 @@
+shoa = int(input('Enter a number: '))
+ertefa = int(input('Enter a number: '))
+hajm = shoa*ertefa*2
+masahat = (shoa*2*ertefa)+(shoa*2**2)
+print('hajm: ',hajm)
+print('masahat: ', masahat)
+
