65e57679f286600e3ef3f7351833ff571c0b4597
yousefmn91 <67608124+yousefmn91@users.noreply.github.com>
2020-08-02 01:59:09 +0430

    Add files via upload

---
+++ 5_1.py
@@ -0,0 +1,6 @@
+ghEmsal=int(input('Enter a number ghEmsal: '))
+ghGhabl=int(input('enter a number ghGhabl: '))
+t=(ghEmsal-ghGhabl)/(ghGhabl*100)
+ghBad=ghEmsal+t*ghEmsal
+print('tavarom: ', t)
+print('gheymat sal bad: ', ghBad)
