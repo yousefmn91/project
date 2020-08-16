2e98f755e0cf9a50c9425315b7d71091c826c4d6
yousefmn91 <67608124+yousefmn91@users.noreply.github.com>
2020-08-16 23:04:03 +0430

    Add files via upload

---
+++ yousef 4.py
@@ -0,0 +1,6 @@
+a=int(input('enter a number: '))
+maliat= int(a*0.07)
+a1=a-maliat
+daraei= int(a1*0.1)
+salary=a-(maliat+daraei)
+print("Hoghoogh {} maliat {} daraei {} hoghoogh nahaei: {}".format(a,maliat,daraei,salary))
